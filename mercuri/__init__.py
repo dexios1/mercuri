1c9bb4aa8492d888cfddac871b16853345eff2da
Chris Dare <christopher.dare@st.vvu.edu.gh>
2018-11-02 13:18:04 +0000

    setup shell context and  entry point for flask app

---
+++ mercuri/__init__.py
@@ -0,0 +1,48 @@
+import os
+
+from flask import Flask, render_template
+from mercuri.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(Config)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from mercuri.blueprints import auth
+    app.register_blueprint(auth.bp)
+
+    # index page
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        return render_template('index.html')
+
+    return app
+
+
+def get_db():
+    return SQLAlchemy(create_app())
+
+
+def get_migrate():
+    return Migrate(create_app(), get_db())
+
+
+# db = SQLAlchemy(create_app())
+# migrate = Migrate(create_app(), db)
