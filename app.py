d697d1e7b904288eb85bedabe59a560e9c6de5cc
Chris Dare <christopher.dare@st.vvu.edu.gh>
2018-11-02 13:16:49 +0000

    setup shell context and  entry point for flask app

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-from app import app
+from mercuri.__init__ import create_app, get_db, get_migrate
+from flask_migrate import Migrate
+
+
+app = create_app()
+db = get_db()
+# register models and push migration
+from mercuri.models.user import User
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
