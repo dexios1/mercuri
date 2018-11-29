f1542cfc5ba8d376a9be8b21106d53fd6e2af784
Chris Dare <christopher.dare@st.vvu.edu.gh>
2018-11-29 21:09:36 +0000

    set up password reset for application
    need to add threading to emailing so the other files on the server can be sent faster whilst an email is being processed to be sense

--- mercuri/blueprints/auth.py
+++ mercuri/blueprints/auth.py
@@ -5,7 +5,10 @@ from flask_login import login_user, current_user, logout_user
 from werkzeug.urls import url_parse
 from mercuri.models import db
 from mercuri.models.user import User
+from mercuri.forms import ResetPasswordRequestForm, ResetPasswordForm
+from mercuri.helpers.email import send_password_reset_email
 from datetime import datetime
+
 
 bp = Blueprint('auth', __name__)
 
@@ -48,6 +51,37 @@ def register():
         flash('Thanks for registering, {}. You can login in now'.format(user.username))
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', title='Register', form=form)
+
+
+@bp.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/reset_password_request.html',
+                           title='Reset Password', form=form)
+
+
+@bp.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('emails/reset_password.html', form=form)
 
 
 @bp.before_request
