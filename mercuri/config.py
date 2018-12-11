0047d3723904adea5031bbf7fd3c7b2a86307c2b
Chris Dare <christopher.dare@st.vvu.edu.gh>
2018-12-11 20:15:12 +0000

    clean config replace os.environ with environ.Env set up config for heroku

--- mercuri/config.py
+++ mercuri/config.py
@@ -1,18 +1,23 @@
-import os
 from flask import Flask
-# basedir = os.path.abspath(os.path.dirname(__file__))
+from environs import Env
 basedir = Flask(__name__).instance_path
+env = Env()
+env.read_env()
 
 
 class Config(object):
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    ENV = env.str('FLASK_ENV', default='production')
+    DEBUG = ENV == 'development'
+    SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
+    SECRET_KEY = env.str('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = env.str('MAIL_SERVER')
+    MAIL_PORT = env.int('MAIL_PORT') or 25
+    MAIL_USE_TLS = env.str('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = env.str('MAIL_USERNAME')
+    MAIL_PASSWORD = env.str('MAIL_PASSWORD')
     ADMINS = ['christopher.dare@st.vvu.edu.gh']
     LANGUAGES = ['en', 'es']
-    SENTRY_URL = "https://bb0a3cdad37040938b082d9a446edfd7@sentry.io/1331904"
+    SENTRY_URL = env.str('SENTRY_URL')
+    #  because we don't want this kind of logging to happen in development but only on heroku:
+    # LOG_TO_STDOUT = env.int('LOG_TO_STDOUT')
